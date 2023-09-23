970afcf44deea425b81246a5a9ae5f542425282f
Hanayou <samuel.short@gmail.com>
2023-09-23 10:01:38 +0100

    Implement animated routes

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { fly } from "svelte/transition";
+    import { page } from "$app/stores";
+</script>
+
+{#key $page.url}
+    <div transition:fly={{x: -100, duration: 250}}>
+        <slot />
+    </div>
+{/key}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
     import "../app.css";
     import NavBar from "$lib/components/NavBar.svelte";
+    import { fly } from "svelte/transition"
+    import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
 </script>
 
 <NavBar></NavBar>
-<slot />
+<AnimatedRoute>
+    <slot />
+</AnimatedRoute>
