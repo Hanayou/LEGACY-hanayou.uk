01e14e89e8c2c6091ccfe934dd7563cadb5d2636
Hanayou <samuel.short@gmail.com>
2023-09-24 17:14:41 +0100

    Remove [[lang]] optional routing param as using localStorage + Stores for managing langauges prefrence

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    export let data;
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
 </script>
 
 <div class="hero min-h-screen" style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
