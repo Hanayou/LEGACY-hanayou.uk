ad77f9360863ec6555dba29af916e5371848981e
Hanayou <samuel.short@gmail.com>
2023-09-26 00:15:37 +0100

    Sanitize input from contact form

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,34 +1,37 @@
 import { fail } from '@sveltejs/kit';
+import sanitize from 'sanitize-html';
 
 export const actions = {
     submit: async ({ cookies, request }) => {
-        // Extract Data
+        // Extract Data and sanitise
         const data = await request.formData();
-        const name = data.get('name');
-        const email = data.get('email');
-        const message = data.get('message');
-        console.log(data);     
+        let name = sanitize(data.get('name') as string);
+        let email = sanitize(data.get('email') as string);
+        let message = sanitize(data.get('message') as string);    
 
-        // Validate
+        console.log({name, email, message});
+
         try {
-            if (name === '') { throw new Error('Name cannot be empty'); }
-            if (email === '') { throw new Error('Email cannot be empty'); }
-            if (message === '') { throw new Error('Message cannot be empty'); }
+            // Validate
+            if (!name) { throw new Error('Name cannot be empty'); }
+            if (!email) { throw new Error('Email cannot be empty'); }
+            if (!message) { throw new Error('Message cannot be empty'); }
             let msgLength = message?.length as number;
             if (msgLength > 1000) { throw new Error('Message must be under 1000 chracters.')}
+
         } catch (error: any) {
             return fail(400, {
-                name: data.get('name'),
-                email: data.get('email'),
-                message: data.get('message'),
+                name: name,
+                email: email,
+                message: message,
                 error: error.message
             });
         }
 
         return {
-            name: data.get('name'),
-            email: data.get('email'),
-            message: data.get('message')
+            name: name,
+            email: email,
+            message: message
         };
 
 
