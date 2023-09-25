86153d80073fb60ff7fb2065858de344def31009
Hanayou <samuel.short@gmail.com>
2023-09-25 04:31:33 +0100

    Trigger page reload once localStorage is accessible to update language

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,17 @@
     import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
     import { onMount } from "svelte";
     import { language } from "$lib/stores/languageStore";
+  import { invalidateAll } from "$app/navigation";
+
+    // Called on first page load or full refresh
+    // Refreshes the page from English to LocalStorage preferred langauge once running on client-side
+    async function onLocalStorageLoad() {
+        await language.getLang();
+        invalidateAll();
+    }
 
     onMount(() => {
-        language.getLang();
+        onLocalStorageLoad();
     });
 </script>
 
