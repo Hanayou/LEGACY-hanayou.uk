8aef3d5763e8032517709b1d7371e350d3b1e73b
Hanayou <samuel.short@gmail.com>
2023-09-29 07:06:03 +0100

    Finish detailed text and translation on about page

--- src/lib/translations/components/about/WorkEducation.ts
+++ src/lib/translations/components/about/WorkEducation.ts
@@ -7,21 +7,21 @@ export const lang = {
                 position: "Consultant",
                 location: "Newcastle, United Kingdom",
                 date: "2023 - Current",
-                content: "This was a great job that I was able to do for a while."
+                content: "Since graduating and doing some travelling over the summer of 2023, I have been freelancing while searching for a job in Japan.<br/><br/>I have recently been involved in developing a document management and version control system for a training company, who have since reapproached me for further freelance work."
             },
             {
                 title: "Northumbria University",
                 position: "BSc Computer Science",
                 location: "Newcastle, United Kingdom",
                 date: "2019 - 2023",
-                content: "This was a great job that I was able to do for a while."
+                content: "I enrolled in Computer Science (with Games Development) at Northumbria University in 2019, undertaking a 4 year course and graduating in the summer of 2023 with first class honours. I undertook this degree while working full-time at Synergi.<br/><br/>My final specialisation was in Games Development which is a personal area of interest to me, however I undertook the same core modules as all other computer science students, with my course consisting of modules such as:<br/><br/><li>Web Development Fundamentals (HTML5, CSS3, JavaScript, PHP)</li><li>Databases and SQL</li><li>Systems Analysis</li><li>Computing Fundamentals (a module discussing low-level architecture and assembly langauge)</li><li>Algorithms and Data Structures</li><li>Operating Systems, Security, and Networking</li><li>Team Project and Professionalism (group teamwork module to deliver a large project)</li><li>GAMES: Game Engine Programming (module covering low-level architecture and development approaches to complex game engine software)</li><li>Final dissertation project</li>"
             },
             {
                 title: "Fukushima University",
                 position: "Exchange Year",
                 location: "Fukushima, Japan",
                 date: "2022",
-                content: "This was a great job that I was able to do for a while."
+                content: "During my 3rd year at Northumbria University, I partook in an exchange at Fukushima University for 6 months (not a full year due to covid). This was an amazing opportunity to live in and explore somewhere new in Japan, as well as engage in using Japanese every day by getting to know the local people and students at the university.<br/><br/>Throughout my time in Fukushima I was able to make great friends with many other international and Japanese students, volunteer in many <a href='https://youtu.be/gUJqb0BWcJo' target='_blank' class='text-primary'>community activities</a> (such as rice farming and charity events) and greatly expand my intercultural collaboration and communication skills.<br/><br/>I was additionally involved in a few club activities outside of classes, primarily COLORs (the intercultural communication club) and the Iaido club."
             },
             {
                 title: "Synergi",
@@ -35,7 +35,7 @@ export const lang = {
                 position: "Intern SE・Technical Evangelist",
                 location: "Melbourne, Australia",
                 date: "2018",
-                content: "Nintex contacted me after seeking someone with both technical and Japanese langauge skills. I joined their main team in Melbourne, Australia in 2018 as an intern out of high school.<br/><br/>Nintex are a very large, international organisation specialising in the development of process automation tools, primarily their automation platform - Nintex Automation Cloud.<br/><br/>My role as a SE/Technical Evangelist requried being a technical and communicative bridge between developers, the sales team, as well as partners and customers developing solutions with the product. Beyond being proficient in all aspects of the product stack, understanding the platform architecture, data flow, and security measures in-depth were a pivotal part of my role to ensure customer trust.<br/><br/>Some key experiences and skills I used while at Nintex include:<br/><li>Cross-department communication and collaboration.</li><li>Running training sessions for customers, partners, and sometimes internal staff on how to use our products.</li><li>Full-stack development of a website that hooked into Nintex's platform API for demonstration at a major marketing event.</li>"
+                content: "Nintex contacted me after seeking someone with both technical and Japanese langauge skills. I joined their main team in Melbourne, Australia in 2018 as an intern out of high school.<br/><br/>Nintex are a very large, international organisation specialising in the development of process automation tools, primarily their automation platform - Nintex Automation Cloud.<br/><br/>My role as a SE/Technical Evangelist requried being a technical and communicative bridge between developers, the sales team, as well as partners and customers developing solutions with the product. Beyond being proficient in all aspects of the product stack, understanding the platform architecture, data flow, and security measures in-depth were a pivotal part of my role to ensure customer trust.<br/><br/>Some key experiences and skills I used while at Nintex include:<br/><br/><li>Cross-department communication and collaboration.</li><li>Running training sessions for customers, partners, and sometimes internal staff on how to use our products.</li><li>Full-stack development of a website that hooked into Nintex's platform API for demonstration at a major marketing event.</li>"
             }
         ]
     },
@@ -47,35 +47,35 @@ export const lang = {
                 position: "コンサルタント・開発者",
                 location: "ニューカッスル, イギリス",
                 date: "2023年 - 現在",
-                content: "結構の時間にできた仕事でした！！！"
+                content: "2023年の夏に卒業して旅行した後、日本での仕事を探しながらフリーランスで仕事をしています。<br/><br/>最近、トレーニング会社向けにドキュメント管理とバージョン管理システムを開発するプロジェクトに関与し、その後、再度フリーランスの仕事を依頼されました。"
             },
             {
                 title: "ノーサンブリア大学",
                 position: "コンピューター・サイエンス学士",
                 location: "ニューカッスル, イギリス",
                 date: "2019年 - 2023年",
-                content: "結構の時間にできた仕事でした！！！"
+                content: "2019年にノーサンブリア大学でコンピューターサイエンス（ゲーム開発専門）に入学し、4年間のコースを受講し、2023年の夏に一等優等で卒業しました。学位を取得しながら、Synergiでフルタイムで働いていました。<br/><br/>最終的な専攻はゲーム開発であり、これは私の個人的な興味の対象ですが、コンピューターサイエンスの他のすべての学生と同じコアモジュールを受講しました。私のコースには以下のようなモジュールが含まれていました：<br/><br/><li>Web開発の基礎（HTML5、CSS3、JavaScript、PHP）</li><li>データベースとSQL</li><li>システム分析</li><li>コンピューティングの基礎（低レベルアーキテクチャとアセンブリ言語に関するモジュール）</li><li>アルゴリズムとデータ構造</li><li>オペレーティングシステム、セキュリティ、ネットワーキング</li><li>チームプロジェクトと専門性（大規模プロジェクトを提供するためのグループワークモジュール）</li><li>GAMES：ゲームエンジンプログラミング（複雑なゲームエンジンソフトウェアの低レベルアーキテクチャと開発アプローチをカバーするモジュール）</li><li>最終論文プロジェクト</li>"
             },
             {
                 title: "福島大学",
                 position: "留年",
                 location: "福島, 日本",
                 date: "2022",
-                content: "結構の時間にできた仕事でした！！！"
+                content: "ノーサンブリア大学の3年生の時、6ヶ月間（COVIDの影響で1年間ではなかった）福島大学での交換留学に参加しました。これは、日本の新しい場所で生活し探索する素晴らしい機会であり、大学の地元の人々や学生と親しくなりながら、日本語を毎日使用する機会でもありました。<br/><br/>福島で時間を通じて、他の多くの留学生や日本人学生と素晴らしい友情を築くことができ、<a href='https://youtu.be/gUJqb0BWcJo' target='_blank' class='text-primary'>地域の活動</a>（例：田植えやチャリティイベントなど）でボランティア活動を行い、異文化間の協力とコミュニケーションスキルを大幅に向上させました。<br/><br/>さらに、授業以外にもいくつかのサークルに参加し、主にCOLORs（異文化コミュニケーションクラブ）と居合道クラブにも関わりました。"
             },
             {
                 title: "Synergi",
                 position: "コンサルタント・開発者",
                 location: "ゲーツヘッド, イギリス",
                 date: "2018 - 2022",
-                content: "Synergiは、イギリスを拠点とするプロセス自動化の専門家であり、Premier Microsoftパートナーです。彼らはカスタムソリューションやMicrosoft PowerPlatformなどのローコード/ノーコードツールを使用して、あらゆる規模の組織がビジネスプロセスを効率化し自動化するのを支援しています。<br/><br/>私がSynergiに参加した当時、それはまだスタートアップ企業であり、チームには約8人しかいませんでした。私はジュニアコンサルタント/デベロッパーとして参加し、コンサルティングチームの中核としてキャリアを築いてきました。<br/><br/>Synergiのコンサルタントの役割は非常に多様であり、要件のスコープ定義から完全な設計、実装、テスト、およびソリューションの展開に至るまで、さまざまな活動が含まれます。<br/><br/>Synergiでの私の経験を通じて、中小企業と大手企業向けにさまざまな種類のソリューションのスコープ定義、設計、開発を行いました。いくつかの製品の例を以下に示します：<br/><br/><li>主要な地域水道会社向けの数百万ドル規模のインシデント報告システムの完全なプロジェクト管理。</li><li>地元の企業向けにSharePoint Onlineの上に完全機能のHRシステムの開発。</li><li>数百万ドル規模のグローバル物流会社向けのCapEx承認システム。</li>"
+                content: "Synergiは、イギリスを拠点とするプロセス自動化の専門家であり、Premier Microsoftパートナーです。カスタムソリューションやMicrosoft PowerPlatformなどのローコード/ノーコードツールを使用して、あらゆる規模の組織がビジネスプロセスを効率化し自動化するのを支援しています。<br/><br/>私がSynergiに参加した当時、それはまだスタートアップ企業であり、チームには約8人しかいませんでした。私はジュニアコンサルタント/デベロッパーとして参加し、コンサルティングチームの中核としてキャリアを築いてきました。<br/><br/>Synergiのコンサルタントの役割は非常に多様であり、要件のスコープ定義から完全な設計、実装、テスト、およびソリューションの展開に至るまで、さまざまな活動が含まれます。<br/><br/>Synergiでの経験を通じて、中小企業と大手企業向けにさまざまな種類のソリューションのスコープ定義、設計、開発を行いました。いくつかの製品の例を以下に示します：<br/><br/><li>主要な地域水道会社向けの数百万ドル規模の事件報告システムの完全なプロジェクト管理。</li><li>地元の企業向けにSharePoint Onlineの上に完全機能のHRシステムの開発。</li><li>数百万ドル規模のグローバル物流会社向けのCapEx承認システム。</li>"
             },
             {
                 title: "Nintex",
                 position: "インターンSE・テクニカル・エバンジェリスト",
                 location: "メルボルン, オーストラリア",
                 date: "2018",
-                content: "Nintexは、テクニカル知識と日本語能力を兼ね備えた人材を探していた後、私に連絡しました。私は高校を卒業してすぐの2018年に、オーストラリアのメルボルンにあるNintexの主要チームにインターンとして参加しました。<br/><br/>Nintexは、主にNintex Automation Cloudと呼ばれる自動化プラットフォームを含むプロセス自動化ツールの開発を専門とする非常に大規模な国際組織です。<br/><br/>私の役割は、SE/テクニカルエバンジェリストとして、開発者、営業チーム、パートナー、および製品を使用したソリューションを開発する顧客とのテクニカル知識とコミュニケーションの橋渡しをすることでした。製品スタックのすべての側面に精通するだけでなく、プラットフォームアーキテクチャ、データフロー、およびセキュリティ対策を詳細に理解することは、顧客の信頼を確保するために私の役割の鍵でした。<br/><br/>Nintexでの経験とスキルの一部は以下のとおりです：<br/><br/><li>部門間のコミュニケーションと協力。</li><li>顧客、パートナー、そして時折内部スタッフ向けに製品の使用方法についてのトレーニングセッションを実施。</li><li>主要なマーケティングイベントでのデモンストレーション用にNintexのプラットフォームAPIに接続したウェブサイトのフルスタック開発。</li>"
+                content: "Nintexは、テクニカル知識と日本語能力を兼ね備えた人材を探していた後、私に連絡しました。高校を卒業してすぐの2018年に、オーストラリアのメルボルンにあるNintexの主要チームにインターンとして参加しました。<br/><br/>Nintexは、主にNintex Automation Cloudと呼ばれる自動化プラットフォームを含むプロセス自動化ツールの開発を専門とする非常に大規模な国際組織です。<br/><br/>私の役割は、SE/テクニカルエバンジェリストとして、開発者、営業チーム、パートナー、および製品を使用したソリューションを開発する顧客とのテクニカル知識とコミュニケーションの橋渡しをすることでした。製品スタックのすべての側面に精通するだけでなく、プラットフォームアーキテクチャ、データフロー、およびセキュリティ対策を詳細に理解することは、顧客の信頼を確保するために私のにとって極めて重要でした。<br/><br/>Nintexでの経験とスキルの一部は以下のとおりです：<br/><br/><li>部門間のコミュニケーションと協力。</li><li>顧客、パートナー、そして時折内部スタッフ向けに製品の使用方法についてのトレーニングセッションを実施。</li><li>主要なマーケティングイベントでのデモンストレーション用にNintexのプラットフォームAPIに接続したウェブサイトのフルスタック開発。</li>"
             }
         ]
     }
